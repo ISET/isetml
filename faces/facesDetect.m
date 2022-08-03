bff3aa439694c5c53872727050e1764c23b85fab
David Cardinal <github@cardinalphoto.com>
2022-08-03 11:40:32 -0700

    Begin adding support for Oxford CNN face detector

--- faces/facesDetect.m
+++ faces/facesDetect.m
@@ -5,21 +5,15 @@ function  [faceOut, foundFaces] = cpFacesDetect(options)
 %
 % D. Cardinal, Stanford University, 2022
 %
-% 
+%
 % Find what we are looking for
 arguments
     options.file = ''; % name of an image file to open & check
     options.image = []; % image object that's already read in
     options.scene = ''; % get a flat image from a scene and evaluate it
     options.interactive = true; % whether to show results
+    options.method = 'VJ' % Viola-Jones Cascade is the default
 end
-
-% Default detector is set for faces
-faceDetect = vision.CascadeObjectDetector();
-
-% merge threshhold impacts accuracy (1 finds tons of things, 4 is default, 8 max?)
-% from some simple experiments, 3 seems like a good compromise
-faceDetect.MergeThreshold = 3;
 
 % Read an image or a video frame or an ISET scene
 if isfile(which(options.file))
@@ -36,8 +30,19 @@ else
     error('Face Detection called with invalid input');
 end
 
-% step asks our detector to look at an image
-foundFaces = step(faceDetect, ourImg);
+switch options.method
+    case 'VJ'
+        % Default detector is set for faces
+        faceDetect = vision.CascadeObjectDetector();
+
+        % merge threshhold impacts accuracy (1 finds tons of things, 4 is default, 8 max?)
+        % from some simple experiments, 3 seems like a good compromise
+        faceDetect.MergeThreshold = 3;
+        % step asks our detector to look at an image
+        foundFaces = step(faceDetect, ourImg);
+    case 'Oxford-CNN'
+        % call its class
+end
 
 % add a rectangle showing any found faces as a box with text
 faceOut = insertObjectAnnotation(ourImg,"rectangle",foundFaces,'Face');
