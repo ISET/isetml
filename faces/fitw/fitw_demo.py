20c4da46b0dfe28df5585ce05c1b68b3b896fc02
David Cardinal <github@cardinalphoto.com>
2022-08-10 11:42:06 -0700

    works!

--- faces/fitw/fitw_demo.py
+++ faces/fitw/fitw_demo.py
@@ -2,64 +2,105 @@
 # Original: https://sefiks.com/2020/08/27/labeled-faces-in-the-wild-for-face-recognition/
 
 # requires matplotlib, sklearn, deepface, dlib
-
+# NOTE: For gpu, install tensorflow-gpu first
+#       the pip install deepface --no-deps
+from sklearn.datasets import fetch_lfw_pairs
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
 import matplotlib.pyplot as plt
 from deepface import DeepFace
-from sklearn.datasets import fetch_lfw_pairs
+from tqdm import tqdm
+import pandas as pd
+
 fetch_lfw_pairs = fetch_lfw_pairs(subset = 'test', color = True, resize = 1)
 
 pairs = fetch_lfw_pairs.pairs
 labels = fetch_lfw_pairs.target
 target_names = fetch_lfw_pairs.target_names
 
-actuals = []; predictions = []; numPredicts = []
-for i in range(0, pairs.shape[0]):
-   pair = pairs[i]
-   img1 = pair[0]
-   img2 = pair[1]
- 
-   # this shows us every result if we want
-   if False:
-      fig = plt.figure()
- 
-      ax1 = fig.add_subplot(1,3,1)
-      plt.imshow(img1)
- 
-      ax2 = fig.add_subplot(1,3,2)
-      plt.imshow(img2)
- 
-      ax3 = fig.add_subplot(1,3,3)
-      plt.text(0, 0.50, target_names[labels[i]])
- 
-      plt.show()
- 
-   #deepface expects bgr instead of rgb
-   img1 = img1[:,:,::-1]; img2 = img2[:,:,::-1]
+instances = pairs.shape[0]
+print("instances: ", instances)
 
-   #Models include: 'Dlib', 'OpenFace'
-   obj = DeepFace.verify(img1, img2
-      , enforce_detection = False, model_name = 'OpenFace', distance_metric = 'euclidean')
-   prediction = obj["verified"]
-   predictions.append(prediction)
-   if prediction == True:
-      numPredicts.append(1)
-   else:
-      numPredicts.append(0)
-   print(numPredicts)
- 
-   actual = True if labels[i] == 1 else False
-   actuals.append(actual)
+from deepface.basemodels import VGGFace, Facenet, OpenFace, FbDeepFace
+from deepface.basemodels.DlibResNet import DlibResNet
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+"""vgg_model = VGGFace.loadModel()
+print("VGG-Face loaded")
+
+facenet_model = Facenet.loadModel()
+print("FaceNet loaded")
+
+openface_model = OpenFace.loadModel()
+print("OpenFace loaded")
+
+deepface_model = FbDeepFace.loadModel()
+print("DeepFace loaded")
+
+"""
+
+dlib_model = DlibResNet()
+print("Dlib loaded")
+
+plot = False
+
+actuals = []; predictions = []; distances = []
+
+pbar = tqdm(range(0, instances))
+
+for i in pbar:
+    pair = pairs[i]
+    img1 = pair[0]; img2 = pair[1]
+    img1 = img1[:,:,::-1]; img2 = img2[:,:,::-1] #opencv expects bgr instead of rgb
+    
+    #obj = DeepFace.verify(img1, img2, model_name = 'VGG-Face', model = vgg_model)
+    #obj = DeepFace.verify(img1, img2, model_name = 'Dlib', model = dlib_model, distance_metric = 'euclidean', enforce_detection=False)
+    obj = DeepFace.verify(img1, img2, enforce_detection=False)
+    prediction = obj["verified"]
+    predictions.append(prediction)
+    
+    distances.append(obj["distance"])
+    
+    label = target_names[labels[i]]
+    actual = True if labels[i] == 1 else False
+    actuals.append(actual)
+    
+    if plot:    
+        print(i)
+        fig = plt.figure(figsize=(5,2))
+
+        ax1 = fig.add_subplot(1,3,1)
+        plt.imshow(img1/255)
+        plt.axis('off')
+
+        ax2 = fig.add_subplot(1,3,2)
+        plt.imshow(img2/255)
+        plt.axis('off')
+
+        ax3 = fig.add_subplot(1,3,3)
+        plt.text(0, 0.50, label)
+        plt.axis('off')
+
+        plt.show()
+
 accuracy = 100*accuracy_score(actuals, predictions)
 precision = 100*precision_score(actuals, predictions)
 recall = 100*recall_score(actuals, predictions)
 f1 = 100*f1_score(actuals, predictions)
 
-print("Accuracy: ", accuracy, ", Precision: ", precision, ", Recall: ", recall, ", F1: ", f1)
-from sklearn.metrics import confusion_matrix
+print("instances: ",len(actuals))
+print("accuracy: " , accuracy, "%")
+print("precision: ", precision, "%")
+print("recall: ", recall,"%")
+print("f1: ",f1,"%")
+
 cm = confusion_matrix(actuals, predictions)
-print(cm)
+cm
  
 tn, fp, fn, tp = cm.ravel()
-print(tn, fp, fn, tp)
+(tn, fp, fn, tp)
+
+"""
+true_negative = 472
+false_positive = 28
+false_negative = 45
+true_positive = 455
+"""
